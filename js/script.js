a5a0c23f7a069e94b9ba22bcb51b4a80bf5cb105
magda <magdalena.aniol@gmail.com>
2018-06-27 17:05:57 +0200

    add quotes app

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
-var url = 'http://api.icndb.com/jokes/random';
-var paragraph = document.getElementById('joke');
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
-	getJoke();
-});
-function getJoke() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-	xhr.addEventListener('load', function(){
-		var response = JSON.parse(xhr.response);
-		paragraph.innerHTML = response.value.joke;
-	});
-	xhr.send();
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var tweetLink = 'https://twitter.com/intent/tweet?text=';
+var quoteUrl = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+function getQuote() {
+	fetch(prefix + quoteUrl, { cache: "no-store" })
+		.then(function(resp) {
+			return resp.json();
+		})
+		.then(createTweet);
 }
-getJoke();
+function createTweet(input) {
+	var data = input[0];
+	var dataElement = document.createElement('div');
+	dataElement.innerHTML = data.content;
+	var quoteText = dataElement.innerText.trim();
+	var quoteAuthor = data.title;
+	if (!quoteAuthor.length) {
+		quoteAuthor = "Unknown author";
+	}
+	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+	if (tweetText.length > 140) {
+		getQuote();
+	} else {
+		var tweet = tweetLink + encodeURIComponent(tweetText);
+		document.querySelector('.quote').innerText = quoteText;
+		document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+		document.querySelector('.tweet').setAttribute('href', tweet);
+	}
+}
+document.addEventListener('DOMContentLoaded', function() {
+	getQuote();
+	document.querySelector('.trigger').addEventListener('click', function() {
+		getQuote();
+	});
+});
+
